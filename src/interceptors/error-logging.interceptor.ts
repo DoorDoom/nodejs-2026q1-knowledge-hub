c6671bf102d5f8ef0d9422e1bdcaffae881f3327
Alina Vagner <ali.vag977@gmail.com>
2026-04-26 21:42:06 +0300

    add prod  logger

--- src/interceptors/error-logging.interceptor.ts
+++ src/interceptors/error-logging.interceptor.ts
@@ -7,12 +7,24 @@ import {
   Logger,
 } from '@nestjs/common';
 import { Request, Response } from 'express';
+import {
+  createWriteStream,
+  existsSync,
+  mkdirSync,
+  rename,
+  stat,
+  WriteStream,
+} from 'fs';
+import * as path from 'path';
 
 @Catch()
 export class GlobalExceptionFilter implements ExceptionFilter {
   private readonly logger = new Logger(GlobalExceptionFilter.name);
+  isProd = process.env.NODE_ENV === 'production';
 
   catch(exception: unknown, host: ArgumentsHost) {
+    let stream = null as WriteStream | null;
+    let logPath = path.join(process.cwd(), 'logs');
     const ctx = host.switchToHttp();
 
     const request = ctx.getRequest<Request>();
@@ -37,6 +49,32 @@ export class GlobalExceptionFilter implements ExceptionFilter {
       `${request.method} ${request.url} ${status} - ${message}`,
       exception instanceof Error ? exception.stack : undefined,
     );
+
+    if (this.isProd) {
+      if (!existsSync(logPath)) {
+        mkdirSync(logPath, { recursive: true });
+      }
+      stream = createWriteStream(path.resolve(logPath, 'app.log'), {
+        flags: 'a+',
+      });
+
+      const time = new Date().toISOString();
+      stream.write(`[${time}] ${message}\n`);
+      stream.close();
+
+      stat(`${logPath}/app.log`, (error, stats) => {
+        if (error) console.log('file log error');
+        if (
+          stats &&
+          (Number(process.env.LOG_MAX_FILE_SIZE) ?? 1024) < stats.size / 1024
+        ) {
+          const newPath = `${logPath}/app-${new Date().toISOString().replace(/:/g, '-')}.log`;
+          rename(`${logPath}/app.log`, newPath, (error) =>
+            console.log('file log error'),
+          );
+        }
+      });
+    }
 
     response.status(status).json({
       success: false,
--- src/interceptors/logging.interceptor.ts
+++ src/interceptors/logging.interceptor.ts
@@ -5,28 +5,71 @@ import {
   CallHandler,
   Logger,
 } from '@nestjs/common';
+import { createWriteStream, existsSync, mkdirSync, WriteStream } from 'fs';
+import { rename, stat } from 'fs/promises';
+import * as path from 'path';
 import { Observable } from 'rxjs';
-import { map, tap } from 'rxjs/operators';
+import { map } from 'rxjs/operators';
 import { sanitize } from 'src/utils/sanitize';
 
 @Injectable()
 export class LoggingInterceptor implements NestInterceptor {
-  private readonly logger = new Logger();
-  levelLog = process.env.LOG_LEVEL || 'log';
+  private readonly logger = new Logger(LoggingInterceptor.name);
+  isProd = process.env.NODE_ENV === 'production';
+  levelLog =
+    !this.isProd && process.env.LOG_LEVEL ? process.env.LOG_LEVEL : 'log';
+
+  writer = {
+    stream: null as WriteStream | null,
+    path: path.join(process.cwd(), 'logs'),
+    maxSize: Number(process.env.LOG_MAX_FILE_SIZE) ?? 1024,
+    initStream: async () => {
+      if (!existsSync(this.writer.path)) {
+        mkdirSync(this.writer.path, { recursive: true });
+      }
+      this.writer.stream = createWriteStream(
+        path.resolve(this.writer.path, 'app.log'),
+        { flags: 'a+' },
+      );
+    },
+    write: async (message: string) => {
+      if (!this.writer.stream) {
+        throw new Error('Log stream not initialized');
+      }
+
+      const time = new Date().toISOString();
+      this.writer.stream.write(`[${time}] ${message}\n`);
+
+      const { size } = await stat(`${this.writer.path}/app.log`);
+      if (this.writer.maxSize < size / 1024) {
+        await this.writer.finishStream();
+        this.writer.initStream();
+      }
+    },
+    finishStream: async () => {
+      if (!this.writer.stream) return;
+      const newPath = `${this.writer.path}/app-${new Date().toISOString().replace(/:/g, '-')}.log`;
+      this.writer.stream.close();
+      await rename(`${this.writer.path}/app.log`, newPath);
+    },
+  };
+
+  constructor() {
+    if (this.isProd) this.writer.initStream();
+  }
 
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const req = context.switchToHttp().getRequest();
-    this.logger[this.levelLog](
-      `[${req.method}] ${req.url} - Incoming request: ${sanitize(req.body)} Params: ${JSON.stringify(req.query)}`,
-    );
+    const msg = `[${req.method}] ${req.url} - Incoming request: ${sanitize(req.body)}\n Params: ${JSON.stringify(req.query)}`;
+    this.logger[this.levelLog](msg);
+    if (this.writer.stream) void this.writer.write(msg);
 
     const now = Date.now();
     return next.handle().pipe(
       map((data) => {
-        this.logger[this.levelLog](
-          `[${req.method}] ${req.url} - Outcoming request: ${sanitize(data)}`,
-        );
-        this.logger[this.levelLog](`Responsed in ${Date.now() - now}ms`);
+        const msg = `[${req.method}] ${req.url}\n - Outcoming request: ${sanitize(data)} (Responsed in ${Date.now() - now}ms)`;
+        this.logger[this.levelLog](msg);
+        if (this.writer.stream) void this.writer.write(msg);
         return data;
       }),
     );
