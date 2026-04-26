947d2ffbc2ec0222fd51c782b1b1269d72086414
Alina Vagner <ali.vag977@gmail.com>
2026-04-26 21:55:12 +0300

    add shoutdown

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,17 @@
 import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  Logger,
+  ValidationPipe,
+} from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { LoggingInterceptor } from './interceptors/logging.interceptor';
 import { GlobalExceptionFilter } from './interceptors/error-logging.interceptor';
 
 async function bootstrap() {
   const isProd = process.env.NODE_ENV === 'production';
+  const logger = new Logger(bootstrap.name);
 
   const app = await NestFactory.create(AppModule, {
     logger: isProd
@@ -40,5 +45,50 @@ async function bootstrap() {
   );
 
   await app.listen(process.env.PORT || 4000);
+
+  process.on('uncaughtException', (error: any, origin: any) => {
+    logger.error({
+      module: 'main process',
+      method: 'uncaughtException',
+      correlationId: 'To be implemented',
+      timestamp: new Date().toISOString(),
+      error: error?.message,
+      stack: error?.stack,
+      origin: origin,
+    });
+  });
+
+  process.on('unhandledRejection', (err: any) => {
+    logger.error({
+      module: 'main process',
+      method: 'unhandledRejection',
+      correlationId: 'To be implemented',
+      timestamp: new Date().toISOString(),
+      error: err?.message,
+    });
+  });
+
+  process.on('exit', (code) => {
+    logger.error({
+      module: 'main process',
+      method: 'exit',
+      correlationId: 'To be implemented',
+      timestamp: new Date().toISOString(),
+      error: code,
+    });
+  });
+
+  process.on('SIGTERM', async () => {
+    logger.log('Received SIGTERM. Shutting down gracefully...');
+    await app.close();
+    process.exit(0);
+  });
+
+  // Handle graceful shutdown on SIGINT (when user interrupts the process)
+  process.on('SIGINT', async () => {
+    logger.log('Received SIGINT. Shutting down gracefully...');
+    await app.close();
+    process.exit(0);
+  });
 }
 bootstrap();
